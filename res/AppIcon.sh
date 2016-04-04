4d7d1c360a57db2a084592b95bf48d257bdfc9eb
Aniket Patil <aniketp563@gmail.com>
2016-04-04 12:03:46 +0530

    Guest User DB Conflict FIX

--- res/AppIcon.sh
+++ res/AppIcon.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-sqlite3 $(sudo find /private/var/folders -name com.apple.dock.launchpad)/db/db "DELETE FROM apps WHERE title='$1';";
+sqlite3 $(sudo find /private/var/folders -name com.apple.dock.launchpad -user $USER)/db/db "DELETE FROM apps WHERE title='$1';";
 
 echo "Reseting Dock ";
 
